a35c26768e5dc7ca58446520c2364a5520074913
7Enten <1907669831@qq.com>
2026-01-28 15:40:20 +0800

    注释掉不必要的内容

--- src/mixins/share.js
+++ src/mixins/share.js
@@ -8,15 +8,15 @@ export default {
        }
     },
     onShareAppMessage(res) { //发送给朋友
-       let shareData = {
-       	title: '邀请好友领取海量现金券!',
-       	desc: '我正在使用xxxApp，赶紧跟我一起来体验！',
-       	link: "https://mall.ichengle.top/uni/",
-       	imgUrl: 'https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-uni-app-doc/d8590190-4f28-11eb-b680-7980c8a877b8.png'
-       }
-       return {
-       	...shareData
-       }
+    //    let shareData = {
+    //    	title: '邀请好友领取海量现金券!',
+    //    	desc: '我正在使用xxxApp，赶紧跟我一起来体验！',
+    //    	link: "https://mall.ichengle.top/uni/",
+    //    	imgUrl: 'https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-uni-app-doc/d8590190-4f28-11eb-b680-7980c8a877b8.png'
+    //    }
+    //    return {
+    //    	...shareData
+    //    }
     },
     onShareTimeline(res) {//分享到朋友圈
         return {
