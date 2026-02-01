a1ffba36b7a08f3ce989b9637905ff1706090d6b
7Enten <1907669831@qq.com>
2026-02-01 12:42:59 +0800

    优化：不要初始化就大量导入

--- src/App.vue
+++ src/App.vue
@@ -34,5 +34,5 @@
 </script>
 
 <style lang="scss">
-	@import '@/sheep/scss/index.scss';
+	// @import '@/sheep/scss/index.scss';
 </style>
--- src/sheep/index.js
+++ src/sheep/index.js
@@ -1,15 +1,16 @@
-import $api from '@/sheep/api';
+// src/sheep/index.js
+// import $api from '@/sheep/api';
 import $url from '@/sheep/url';
 import $router from '@/sheep/router';
 import $platform from '@/sheep/platform';
 import $helper from '@/sheep/helper';
 import zIndex from '@/sheep/config/zIndex.js';
-import $store from '@/sheep/store';
-import dayjs from 'dayjs';
+// import $store from '@/sheep/store';
+// import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import duration from 'dayjs/plugin/duration';
 import 'dayjs/locale/zh-cn';
-import {Base64} from "js-base64";
+// import {Base64} from "js-base64";
 
 dayjs.locale('zh-cn');
 dayjs.extend(relativeTime);
@@ -28,10 +29,10 @@ const sheep = {
 // 加载Shopro底层依赖
 export async function ShoproInit() {
   // 应用初始化
-  await $store('app').init();
+  // await $store('app').init();
 
   // 平台初始化加载(各平台provider提供不同的加载流程)
-  $platform.load();
+  // $platform.load();
 
   // if (process.env.NODE_ENV === 'development') {
   //   ShoproDebug();
